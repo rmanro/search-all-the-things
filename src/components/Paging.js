f24029654e1498d14db8c3eb205281ca2e0ccb69
Ryan Manro <rmanro@gmail.com>
2018-05-31 10:37:35 -0700

    css styling

--- src/components/Paging.js
+++ src/components/Paging.js
@@ -18,10 +18,8 @@ export default class Paging extends Component {
       <section>
       <div>
         <span>Page {page} of {totalPages}</span>
-        &nbsp;&nbsp;
         <button onClick={() => this.handlePage(-1)} disabled={page === 1}>&lt; Prev</button>
         <button onClick={() => this.handlePage(1)} disabled={page === totalPages}>Next &gt;</button>
-        &nbsp;&nbsp;
         <span>{totalItems} Total Books</span>
       </div>
       <div>
