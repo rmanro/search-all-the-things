ad4d0f782b1712bb9c0142faf528f0e2db3ab711
Ryan Manro <rmanro@gmail.com>
2018-06-03 15:11:33 -0700

    added About placeholder component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
-// import About from './About';
+import About from './About';
 // import Search from '../search/Search';
 // import BookDetail from '../books/BookDetail';
 import './App.css';
@@ -18,8 +18,8 @@ export default class App extends Component {
           <main>
             {<Switch>
               <Route exact path="/" component={Home}/>
-              {/* <Route path="/about" component={About}/>
-              <Route path="/search" component={Search}/>
+              <Route path="/about" component={About}/>
+              {/* <Route path="/search" component={Search}/>
               <Route path="/books/:id" render={({ match, history }) => {
                 return <BookDetail gbID={match.params.id} history={history}/>;
               }}/> */}
