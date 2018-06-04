141f4d5669d6cc4bf2a00565380d265f47b1a34f
Ryan Manro <rmanro@gmail.com>
2018-06-04 15:45:14 -0700

    refactoring header html

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Header.css';
 
 export default class Header extends Component {
@@ -14,7 +14,6 @@ export default class Header extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/search">Search</Link></li>
-            <Route path="/books" render={() => <li>only on books!</li>}/>
           </ul>
         </nav>
       </header>
