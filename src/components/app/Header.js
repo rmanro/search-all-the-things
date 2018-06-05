fbea3f9c24567f948e25582ee9279402a7b4db50
Ryan Manro <rmanro@gmail.com>
2018-06-04 17:10:30 -0700

    home styling

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from './Header.css';
 import home from './home.png';
 import about from './about.png';
+import search from './search.png';
 
 export default class Header extends Component {
   render() {
@@ -13,9 +14,9 @@ export default class Header extends Component {
         </section>
         <nav>
           <ul>
-            <li><Link to="/"><img src={home}/></Link></li>
-            <li><Link to="/about"><img src={about}/></Link></li>
-            <li><Link to="/search">Search</Link></li>
+            <li><Link to="/search"><img src={search}/></Link></li>
+            <li className="about-icon"><Link to="/about"><img src={about}/></Link></li>
+            <li className="home-icon"><Link to="/"><img src={home}/></Link></li>
           </ul>
         </nav>
       </header>
