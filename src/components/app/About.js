37a24e991bb7dd72e7655499f9c5fb06de83dc8d
Ryan Manro <rmanro@gmail.com>
2018-06-05 10:44:40 -0700

    rewrote about page

--- src/components/app/About.js
+++ src/components/app/About.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import styles from './About.css';
 
 export default class About extends Component {
   render() {
-    return <div>Search for books by Title or Author!</div>;
+    return <div className={styles.about}><p>Search for books by author or title.</p><p>Uses the Google Book API.</p><p>Made by Ryan Manro using React.</p></div>;
   }
 }
